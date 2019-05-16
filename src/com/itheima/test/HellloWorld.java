675c873a9442837dd953516f16f814e360484cb6
linyao <2507995429@qq.com>
2019-05-16 20:39:05 +0800

    第一次提交

---
+++ src/com/itheima/test/HellloWorld.java
@@ -0,0 +1,13 @@
+package com.itheima.test;
+
+/**
+ * @program: git_test
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2019-05-16 20:18
+ **/
+public class HellloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorid");
+    }
+}
